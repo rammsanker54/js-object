81c0a5adcb4f87a9ec4c8e261c4472717e83bbab
rammsanker <109334253+rammsanker54@users.noreply.github.com>
2022-08-31 22:10:24 +0530

    Add files via upload

---
+++ js object/js/main.js
@@ -0,0 +1,129 @@
+var laptop = {
+        brand: "acer",
+        model: "gateway",
+        color:"black",
+        size: "14inch"
+}
+console.log(laptop)
+var tv = {
+    brand: "mi",
+    model: "xperial",
+    color: "black",
+    size:"43"
+}
+{console.log(tv.model)}
+
+//
+
+var bike = {
+    brand: "bajaj",
+    model: "pulsar",
+    color:"black",
+    fueltype: "petrol"
+}
+console.log(bike)
+//
+var mobile = {
+    brand: "oppo",
+    model: "a31",
+    color:"silver",
+    size: "6inch"
+}
+console.log(mobile);
+//
+
+var watch = {
+    brand: "rolex",
+    model: "a20",
+    color:"gold",
+    type: "analog"
+}
+console.log(watch);
+
+//
+
+var headphone = {
+    brand: "boat",
+    model: "bluetooth",
+    color:"black",
+    
+}
+console.log(bike)
+//
+//
+
+var shirt = {
+    brand: "peterengland",
+    model: "fullsleeve",
+    color:"navyblue",
+    fabric: "linen"
+}
+console.log(shirt);
+//
+//
+
+var fan = {
+    brand: "usha",
+    model: "3speed",
+    color:"silver",
+    type: "ceiling"
+}
+console.log(fan);
+//
+var building={
+  size: "100sq",
+  height: "20feet",
+  floors: "3floors",
+  color :"green"
+}
+console.log(building);
+//
+var door={
+    size: "2feet",
+    height: "180cm",
+    material: "wood",
+    color :"brown"
+  }
+  console.log(door);
+  //
+  var book={
+    size: "100pg",
+    height: "10cm",
+    price: "150rs",
+    cover :"photo"
+  }
+  console.log(book);
+  //
+  var person={
+    name: "ramm",
+    height: "5.5feet",
+  qualification: "graduate",
+    color :"black"
+  } 
+  console.log(person);
+  //
+  var footwear={
+    model: "slides",
+    size: "8",
+    price: "500rs",
+    color :"black"
+  } 
+  console.log(footwear);
+//
+var vechicle={
+    model: "lorry",
+    fueltype: "diesel",
+    tyre: "6",
+    kmdriven :"10,000miles"
+  } 
+  console.log(footwear);
+//
+var cycle = {
+    model: "turbodrive",
+    size: "8",
+    price: "8500rs",
+    color :"black"
+  } 
+  console.log(cycle);
+
+//
